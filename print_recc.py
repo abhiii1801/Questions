4ce494a17e91e8931bdece4392ffae416ac9038f
abhiii1801 <140613897+abhiii1801@users.noreply.github.com>
2025-08-24 21:58:29 +0530

    added similar solutions

--- print_recc.py
+++ print_recc.py
@@ -10,7 +10,7 @@ class Solution:
 
         _print(n,1)
             
-#https://www.geeksforgeeks.org/problems/print-n-to-1-without-loop/1
+#https://www.geeksforgeeks.org/problems/print-n-to-1-without-loop/
             
 class Solution:
     def printNos(self, n):
